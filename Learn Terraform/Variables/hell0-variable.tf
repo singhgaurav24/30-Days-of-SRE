cf845270ffff25150fea718db6b2d4b170941544
Gaurav Kumar Singh <gauravkumar.singh@vodafone.com>
2024-07-31 21:35:14 +0100

    Terraform

---
+++ Learn Terraform/Variables/hell0-variable.tf
@@ -0,0 +1,9 @@
+# variable username {}
+
+# output prinatname {
+#     value = var.username
+# }
+
+# output prinatname1 {
+#     value = "Hello!, ${var.username}"
+# }
---
+++ Learn Terraform/Variables/test2.tf
@@ -0,0 +1,3 @@
+output print {
+  value = var.myname
+}
