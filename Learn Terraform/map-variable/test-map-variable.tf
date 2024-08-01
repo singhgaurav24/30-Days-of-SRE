eb5c00fb417fb22299a7df920fc3ce60547f4809
Gaurav Kumar Singh <gauravkumar.singh@vodafone.com>
2024-08-01 18:59:45 +0100

    Terraform

---
+++ Learn Terraform/map-variable/test-map-variable.tf
@@ -0,0 +1,17 @@
+# variable "userAge" {
+#   type = map
+#   default = {
+#     Gaurav = 26
+#     Amrit  = 25
+#     Praksh = 24
+#     Akshit = 23
+#   }
+# }
+
+# variable "userName" {
+#   type = string
+# }
+
+# output "userAge" {
+#   value = "Age of ${var.userName} ${lookup(var.userAge, "${var.userName}")}"
+# }
